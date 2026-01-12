7d594931cd30ecda4a30908f452db7ef5eb83108
Layla090 <skoka2028@visi.org>
2026-01-12 03:12:26 +0000

    starting code

---
+++ start.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 6
+#define NUM_LEDS 7
+
+void setup() {
+    pixels.begin();
+    pixels.setBrightness(40);
+    pixels.shoe();
+}
+
+void loop() {
+    
+}
